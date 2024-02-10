b031e1f5e8775c5899dc5c66dbc4a27eafccb898
OrionDooms <orion45.dd@gmail.com>
2024-02-10 13:00:40 -0800

    base_model test case

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,27 +1,44 @@
 #!/usr/bin/python3
-""" """
+"""unittest"""
 import unittest
 from models.base_model import BaseModel
+from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
+    """BaseModel testcase"""
 
-    def test_init(self):
+    def test_uuid(self):
+        """testing uuid"""
         base1 = BaseModel()
         base2 = BaseModel()
-        # Ensure that thei uuid are unique
+        # Ensure that the uuid are unique
         self.assertNotEqual(base1.id, base2.id)
-        self.assertIsNotNone(base1.created_at)
-        self.assertIsNotNone(base2.updated_at)
+        self.assertIsInstance(base1, BaseModel)
+        self.assertTrue(base1.id)
+        self.assertIsNotNone(base2.id)
+
+    def test_created_at(self):
+        """testing created_at"""
+        base1 = BaseModel()
+        self.assertIsInstance(base1.created_at, datetime)
+
+    def test_updated_at(self):
+        """testing updated_at"""
+        base1 = BaseModel()
+        self.assertIsInstance(base1.updated_at, datetime)
 
     def test_save(self):
-        pass
+        """testing save"""
+        base1 = BaseModel()
+        base2 = BaseModel()
+        self.assertIsNotNone(base1.save)
+        self.assertNotEqual(base1.save, base2.save)
 
     def test_to_dict(self):
-        pass
-
-    def test_str(self):
-        pass
+        """testing to_dict"""
+        base1 = BaseModel()
+        self.assertIsNotNone(base1.to_dict)
 
 
 if __name__ == "__main__":
