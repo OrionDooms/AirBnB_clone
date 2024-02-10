f7bb78a18ee37e0f055ff3976b2ba37ae9312557
OrionDooms <orion45.dd@gmail.com>
2024-02-10 15:39:33 -0800

    file_storage test case

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""unittest"""
+import unittest
+from models.base_model import BaseModel
+import models
+from models.engine.file_storage import FileStorage
+
+class testFileStorage(unittest.TestCase):
+    """FileStorage testcase"""
+
+    def test_all(self):
+        pass
+
+    def test_save(self):
+        pass
+
+    def test_reload(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
